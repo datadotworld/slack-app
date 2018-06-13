0b1345a4c87320250617a7bc8fd17aa36b7cb100
Kehinde Shittu <kehesjay@gmail.com>
2018-06-13 13:41:35 +0100

    Adds copyright text.

--- server/controllers/command.js
+++ server/controllers/command.js
@@ -1,3 +1,22 @@
+/*
+ * Data.World Slack Application
+ * Copyright 2018 data.world, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * This product includes software developed at
+ * data.world, Inc. (http://data.world/).
+ */
 const Channel = require("../models").Channel;
 const Subscription = require("../models").Subscription;
 const User = require("../models").User;
@@ -234,12 +253,12 @@ const belongsToChannelAndUser = async (resourceid, channelid, userId) => {
 };
 
 const listSubscription = async (req, token) => {
-  // let command = req.body.command + req.body.text;
   let responseUrl = req.body.response_url;
   let channelid = req.body.channel_id;
   let userId = req.body.user_id;
 
   try {
+    //Get all subscriptions in this channel
     const subscriptions = await Subscription.findAll({
       where: { channelId: channelid }
     });
@@ -327,7 +346,6 @@ const removeSubscriptionRecord = (owner, id, userId) => {
   });
 };
 
-//TODO : This needs to be refactored.
 const extractParamsFromCommand = (command, isAccountCommand) => {
   let params = {};
   let parts = command.split(" ");
