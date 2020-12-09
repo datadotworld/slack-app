3a2d51c642c45372d42629977853b89ad29501bc
danielpeng2 <danielpeng2@gmail.com>
2020-12-09 16:06:18 -0500

    edit help message to include webhook command

--- server/controllers/command.js
+++ server/controllers/command.js
@@ -623,7 +623,8 @@ const showHelp = async responseUrl => {
     `_Unsubscribe from a data.world dataset:_ \n \`/${commandText} unsubscribe dataset_url\``,
     `_Unsubscribe from a data.world project:_ \n \`/${commandText} unsubscribe project_url\``,
     `_Unsubscribe from a data.world account:_ \n \`/${commandText} unsubscribe account\``,
-    `_List active subscriptions._ : \n \`/${commandText} list\``
+    `_List active subscriptions._ : \n \`/${commandText} list\``,
+    `_Get a webhook URL for the current channel:_ \n \`/${commandText} webhook\``
   ];
 
   collection.forEach(commandsInfo, value => {
