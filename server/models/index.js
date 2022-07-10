87742eee8a21b1c640fbaf7afcdfdbfe5e243cb8
nikhilhegde-ddw <nikhil.hegde@data.world>
2022-07-10 17:15:18 +0530

    index change

--- server/models/index.js
+++ server/models/index.js
@@ -5,15 +5,22 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL, { logging : false });
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  const options = {
+    host: "127.0.0.1",
+    dialect: "postgres"
+  };
+  sequelize = new Sequelize(
+    process.env.PG_DATABASE,
+    process.env.PG_USERNAME,
+    process.env.PG_PASSWORD,
+    options
+  );
 }
 
 fs
